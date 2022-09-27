c863a40af7cf18a30cd77080e934e31ef043793a
Cyril <cyril.ghali@hotmail.com>
2022-09-27 15:23:55 +0200

    analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,15 @@ class MyDocument extends Document {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Herr+Von+Muellerhoff&display=swap" rel="stylesheet" /> 
+        <script
+          async
+          id="chiffre:analytics"
+          src="https://chiffre.io/analytics.js"
+          data-chiffre-project-id="CftpjbMo3wqA2GBo"
+          data-chiffre-public-key="pk.wcpsC0AJieEYzKdLlQWx2h0AFhQvpCHne05ehkJnA0k"
+          referrerpolicy="origin"
+          crossOrigin="anonymous"
+        />
         </Head>
         <body>
           <Main />
