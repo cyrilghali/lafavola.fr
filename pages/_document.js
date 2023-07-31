4bc3b6c9589e7769b4c0a4cda8df7cfe30ba81be
Cyril GHALI <accounts@cyrilghali.fr>
2023-07-31 17:16:50 +0200

    dark scheme

--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,37 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Herr+Von+Muellerhoff&display=swap" rel="stylesheet" /> 
-        <script
-          async
-          id="chiffre:analytics"
-          src="https://chiffre.io/analytics.js"
-          data-chiffre-project-id="CftpjbMo3wqA2GBo"
-          data-chiffre-public-key="pk.wcpsC0AJieEYzKdLlQWx2h0AFhQvpCHne05ehkJnA0k"
-          referrerPolicy="origin"
-          crossOrigin="anonymous"
-        />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Herr+Von+Muellerhoff&display=swap"
+            rel="stylesheet"
+          />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-T6CJ58PPKP"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'G-T6CJ58PPKP');
+          </script>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
